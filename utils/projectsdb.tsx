294db6bc439338a0cf9c19ba18384f4bc568fe79
areeba-12 <159996043+areeba-12@users.noreply.github.com>
2024-11-08 19:07:01 +0000

    Update projectsdb.tsx

--- utils/projectsdb.tsx
+++ utils/projectsdb.tsx
@@ -1,5 +1,6 @@
 export const projects = [
-    {
+    { 
+        id: '1',
       title: "Project 1",
       description: "Create one page website using NextJs and Tailwind Css.",
       image: "/projects1.png",
@@ -7,12 +8,14 @@ export const projects = [
       githubLink: "https://github.com/areeba-12/syeda-areeba-portfolio.git",
     },
     {
+        id: '2',
       title: "Project 2",
       description: "Create a clone of Panaverse.",
       image: "/projects2.png",
       githubLink: "https://github.com/areeba-12/panaverse.git",
     },
     {
+        id: '3',
       title: "Project 3",
       description: "Created Resume Builder using HTML, CSS and Typescript.",
       image: "/projects3.png",
@@ -20,6 +23,7 @@ export const projects = [
       githubLink: "https://github.com/areeba-12/resume_builder.git",
     },
     {
+        id: '4',
       title: "Project 4",
       description: "Created Resume Builder using HTML, CSS and Typescript.",
       image: "/projects4.png",
